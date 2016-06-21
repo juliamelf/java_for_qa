ad957ab76442d43bd8dc57b48567ca57b0159c80
juliamelf <juliamelf@gmail.com>
2016-06-21 22:17:13 +0300

    contact creation tests updated with lambda

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/ContactCreationTests.java
@@ -17,13 +17,7 @@ public class ContactCreationTests extends TestBase {
         List<ContactData> after = app.getContactHelper().getContactList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        int max = 0;
-        for (ContactData c: after) {
-            if (c.getId() > max) {
-                max = c.getId();
-            }
-        }
-        contact.setId(max);
+        contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(contact);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
@@ -18,15 +18,6 @@ public class GroupCreationTests extends TestBase {
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size() + 1);
 
-        /*
-        int max = 0;
-        for (GroupData g: after) {
-            if (g.getId() > max) {
-                max = g.getId();
-            }
-        }
-*/
-
         group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
