5cafeb05321cbcdcc34f816de005108ce7e67ba7
juliamelf <juliamelf@gmail.com>
2016-06-22 17:02:03 +0300

    tests run in suite

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/TestBase.java
@@ -2,7 +2,9 @@ package ru.juliamelf.javaqa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import ru.juliamelf.javaqa.addressbook.appmanager.ApplicationManager;
 
 /**
@@ -12,12 +14,12 @@ public class TestBase {
 
     public final static ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterMethod
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
