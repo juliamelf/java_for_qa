238a808f0f33f17ff88347f668afd3d8286915e5
juliamelf <juliamelf@gmail.com>
2016-07-03 13:53:14 +0300

    Added parameters to GroupCreation tests.

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,32 @@
 package ru.juliamelf.javaqa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.juliamelf.javaqa.addressbook.model.GroupData;
 import ru.juliamelf.javaqa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroups() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        list.add(new Object[] {new GroupData().withName("group1").withHeader("header1").withFooter("footer1")});
+        list.add(new Object[] {new GroupData().withName("group2").withHeader("header2").withFooter("footer2")});
+        list.add(new Object[] {new GroupData().withName("group3").withHeader("header3").withFooter("footer3")});
+        return list.iterator();
+    }
+
+    @Test(dataProvider = "validGroups")
+    public void testGroupCreation(GroupData group) {
         app.goTo().GroupPage();
         Groups before = app.group().all();
-        GroupData group = new GroupData().withName("test group");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
         Groups after = app.group().all();
@@ -21,7 +34,7 @@ public class GroupCreationTests extends TestBase {
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test(enabled = false)
     public void testBadGroupCreation() {
         app.goTo().GroupPage();
         Groups before = app.group().all();
