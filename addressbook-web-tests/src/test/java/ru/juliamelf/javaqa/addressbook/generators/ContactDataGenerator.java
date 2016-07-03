8df7a775770492cbe92e2575628a1cf703681fe4
juliamelf <juliamelf@gmail.com>
2016-07-03 12:44:02 +0300

    jCommander added.

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.juliamelf.javaqa.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.juliamelf.javaqa.addressbook.model.ContactData;
 
 import java.io.*;
@@ -11,13 +14,28 @@ import java.util.List;
  */
 public class ContactDataGenerator {
 
+    @Parameter(names = "-c", description = "Contact count")
+    public int count;
+
+    @Parameter(names = "-f", description = "Target file")
+    public String file;
+
     public static void main (String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
 
+        try {
+            jCommander.parse(args);
+        } catch (ParameterException ex) {
+            jCommander.usage();
+            return;
+        }
+        generator.run();
+    }
+
+    private void run() throws IOException {
         List<ContactData> contacts = generateContacts(count);
-        save(contacts, file);
-
+        save(contacts, new File(file));
     }
 
     private static void save(List<ContactData> contacts, File file) throws IOException {
