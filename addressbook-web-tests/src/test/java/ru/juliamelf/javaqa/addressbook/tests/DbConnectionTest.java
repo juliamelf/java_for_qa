52ab0982fa085a8e630fadcf2a2c7cdd91033985
juliamelf <juliamelf@gmail.com>
2016-07-10 15:00:02 +0300

    DB connection added.

---
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/DbConnectionTest.java
@@ -0,0 +1,42 @@
+package ru.juliamelf.javaqa.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.juliamelf.javaqa.addressbook.model.GroupData;
+import ru.juliamelf.javaqa.addressbook.model.Groups;
+
+import java.sql.*;
+
+/**
+ * Created by Julia on 7/10/2016.
+ */
+public class DbConnectionTest {
+
+    @Test
+    public void testDbConnection () {
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook?user=root&password=");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
+            Groups groups = new Groups();
+            while(rs.next()) {
+                groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
+                        .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
+            }
+            rs.close();
+            st.close();
+            conn.close();
+            System.out.println(groups);
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+    }
+}
