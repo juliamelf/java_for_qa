8f75c599da2438c2614b3a62922c5b2cb1376838
juliamelf <juliamelf@gmail.com>
2016-07-10 18:45:37 +0300

    Groups tests updated.

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
@@ -60,22 +60,22 @@ public class GroupCreationTests extends TestBase {
     @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) {
         app.goTo().GroupPage();
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size() + 1));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test(enabled = false)
+    @Test()
     public void testBadGroupCreation() {
         app.goTo().GroupPage();
-        Groups before = app.group().all();
+        Groups before = app.db().groups();
         GroupData group = new GroupData().withName("test group'");
         app.group().create(group);
         assertThat(app.group().count(), equalTo(before.size()));
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before));
     }
 }
--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupDeletionTests.java
@@ -13,19 +13,20 @@ public class GroupDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
-        app.goTo().GroupPage();
-        if(app.group().all().size() == 0) {
+        if(app.db().groups().size() == 0) {
+            app.goTo().GroupPage();
             app.group().create(new GroupData().withName("test group"));
         }
     }
 
     @Test
     public void testGroupDeletion() {
-        Groups before = app.group().all();
+        app.goTo().GroupPage();
+        Groups before = app.db().groups();
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);
         assertEquals(app.group().count(), before.size() - 1);
-        Groups after = app.group().all();
+        Groups after = app.db().groups();
         assertThat(after, equalTo(before.without(deletedGroup)));
 
     }
