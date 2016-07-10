b604f26e1909f4e8adf167cd8f103712d079ac5d
juliamelf <juliamelf@gmail.com>
2016-07-10 13:42:45 +0300

    Logging added.

--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/GroupCreationTests.java
@@ -3,6 +3,8 @@ package ru.juliamelf.javaqa.addressbook.tests;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.juliamelf.javaqa.addressbook.model.GroupData;
@@ -20,6 +22,8 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+
+
 
     @DataProvider
     public Iterator<Object[]> validGroupsFromXml() throws IOException {
--- addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/juliamelf/javaqa/addressbook/tests/TestBase.java
@@ -1,14 +1,23 @@
 package ru.juliamelf.javaqa.addressbook.tests;
 
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.juliamelf.javaqa.addressbook.appmanager.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by korotkova on 5/30/2016.
  */
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     public final static ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,9 +27,19 @@ public class TestBase {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @BeforeMethod
+    public void logTestRun(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
     }
 
 
